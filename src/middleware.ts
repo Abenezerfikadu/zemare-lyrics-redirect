54f252d3de692efc26deee03960921c3b75dbf35
Abenezerfikadu <abenezerfikadu76@gmail.com>
2025-02-06 14:43:27 +0300

    allow access to assetlinks.json in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,10 @@ export function middleware(request: NextRequest) {
   console.log("Middleware executed for:", pathname);
 
   if (pathname === "/.well-known/assetlinks.json") {
-    return new Response("Forbidden", { status: 403 });
+    if (pathname === "/.well-known/assetlinks.json") {
+      return NextResponse.next();
+    }
+
   }
 
   return NextResponse.next();
